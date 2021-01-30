f59e45fbc526d255710f45930df95777098daf07
kelvin.gikonyo <kevohm.km@gmail.com>
2021-01-30 11:38:39 +0300

    altered routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use \App\Http\Controllers\GeneralController;
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 
-    // Route::domain('admin.thebrainerke.co.ke')->group(function () {
+    Route::domain('admin.thebrainerke.co.ke')->group(function () {
 
         Route::get('/', [AdminController::class, 'admin'])->name('dashboard');
         Route::get('/manage', [AdminController::class, 'manage'])->name('manage');
@@ -68,7 +68,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
         Route::get('manage-categories', [AdminController::class, 'manageCategories'])->name('manage-categories');
         Route::post('save-categories', [AdminController::class, 'saveCategories'])->name('save-category');
 
-    // });
+    });
 
 });
 
