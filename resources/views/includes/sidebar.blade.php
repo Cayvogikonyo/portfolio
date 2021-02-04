901f847b7f829f80ee51ff8af6cdfaf99f04ffe8
kelvin.gikonyo <kevohm.km@gmail.com>
2021-02-04 14:04:34 +0300

    added theme support

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -9,4 +9,9 @@
         </div>
         
     </div>
+    <button data-uid="null" id="themebtn" title="Toggle Theme" class="m-3 pop-prop text-lg text-black dark:text-white underline font-bold px-3 p-1 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 absolute bottom-0 left-0"><i class="fas fa-sun" id="themeIcon"></i></button>
+    <div id="tooltipnull" class="hidden w-1/6 absolute popper px-2 py-3 rounded z-20 relative bg-gray-900 dark:bg-indigo-900 text-white" role="tooltip">
+        Toggle theme
+        <div class="arrow absolute h-4 w-4 text-yellow-600 arrow-light dark:arrow-dark text-white" data-popper-arrow></div>
+    </div>
 </div>
